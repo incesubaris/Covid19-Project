52b2bd51fc7eec8c2c42002dae1603601bbee05a
incesubaris <barisincesu@outlook.com>
2020-12-18 00:11:04 +0300

    basic INSERT INTO added

--- server.py
+++ server.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template
-from datetime import datetime
-
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 
-@app.route('/')
-def homepage():
-    today = datetime.today()
-    day_name = today.strftime("%A")
-    return render_template("home.html", day=day_name)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'Abcd.1234'
+app.config['MYSQL_DB'] = 'BLG317'
+
+mysql = MySQL(app)
 
 
-if __name__ == "__main__":
-    app.run(host="localhost", port=8080, debug=True)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
